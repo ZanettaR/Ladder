e49b8993691efaabb3d44c6defff62584aaa6c18
heavenlypeach <mubiynacasey@aol.com>
2017-07-15 12:42:51 -0700

    sets up root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'homepages/landing'
+  root 'homepages#landing'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
