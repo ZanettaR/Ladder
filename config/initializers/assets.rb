b25a6a84ff08e33ae14eb1dfceb32f9a617ff152
Maya Ramsey <Maya.ramsey@gmail.com>
2017-07-15 22:20:22 -0700

    I'm not sure what this does

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+<<<<<<< Updated upstream
+=======
+Rails.application.config.assets.precompile += %w( profile.css )
+Rails.application.config.assets.precompile += %w( signin.css )
+>>>>>>> Stashed changes
